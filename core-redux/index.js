7bed85ed236e5383e75bf6aca5f43fa4ed79c6fc
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-21 10:19:10 +0500

    own practice core redux

--- core-redux/index.js
+++ core-redux/index.js
@@ -1,27 +1,58 @@
-const redux = require("redux");
+// const redux = require("redux");
   
-const shopReducer = (state = {soap: 10}, action)  => {
-    // state.soap--   Incorrect way
-    if(action.type === "Sell_Soap"){
+// const shopReducer = (state = {soap: 10}, action)  => {
+//     // state.soap--   Incorrect way
+//     if(action.type === "Sell_Soap"){
+//         return {
+//             soap: state.soap - 1
+//         };
+//     }
+//     else if(action.type === "Buy_Soap"){
+//         return {
+//             soap: state.soap + action.payload
+//         };
+//     }
+//     return state;
+// }
+
+// const store = redux.createStore(shopReducer)
+
+// const subcriber = () => {
+//     const state = store.getState();
+//     console.log(state);
+// }
+
+// store.subscribe(subcriber);
+// store.dispatch({type:"Sell_Soap"})
+// store.dispatch({type:"Buy_Soap", payload: 10})
+
+
+
+// Own Practice
+const redux = require("redux");
+
+const reducer = (state , action) =>{
+    if(action.type === "Product_Sell"){
         return {
-            soap: state.soap - 1
-        };
+            products: state.products - 1,
+        }
     }
-    else if(action.type === "Buy_Soap"){
+    if(action.type === "Product_Buy"){
         return {
-            soap: state.soap + action.payload
-        };
+            products: state.products + action.payload,
+        }
     }
     return state;
 }
 
-const store = redux.createStore(shopReducer)
+const store = redux.createStore(reducer,{products: 10});
 
-const subcriber = () => {
+const subcribetion = () => {
     const state = store.getState();
     console.log(state);
 }
+store.subscribe(subcribetion);
+store.dispatch({type:"Product_Sell"});
+store.dispatch({type:"Product_Buy", payload: 10});
 
-store.subscribe(subcriber);
-store.dispatch({type:"Sell_Soap"})
-store.dispatch({type:"Buy_Soap", payload: 10})
+
