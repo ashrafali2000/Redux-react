0c18b4322aa44e471f97c8af66ff45220259c89a
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-21 14:08:20 +0500

    indexjs update

--- redux-toolkit/src/index.js
+++ redux-toolkit/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
 import reportWebVitals from './reportWebVitals';
+import { store } from './store';
+import { Provider } from "react-redux"
 import Home from './Home';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  <Home></Home>
-  </React.StrictMode>
+  <Provider store={store}>
+
+  {/* <React.StrictMode> */}
+<Home />
+  {/* </React.StrictMode> */}
+
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
