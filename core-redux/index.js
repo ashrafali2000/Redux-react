5352b7f0c0c2417f74619051445fadae4786032c
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-21 10:04:40 +0500

    redux create and update

--- core-redux/index.js
+++ core-redux/index.js
@@ -1,1 +1,27 @@
-console.log("my Check redux")
+const redux = require("redux");
+  
+const shopReducer = (state = {soap: 10}, action)  => {
+    // state.soap--   Incorrect way
+    if(action.type === "Sell_Soap"){
+        return {
+            soap: state.soap - 1
+        };
+    }
+    else if(action.type === "Buy_Soap"){
+        return {
+            soap: state.soap + action.payload
+        };
+    }
+    return state;
+}
+
+const store = redux.createStore(shopReducer)
+
+const subcriber = () => {
+    const state = store.getState();
+    console.log(state);
+}
+
+store.subscribe(subcriber);
+store.dispatch({type:"Sell_Soap"})
+store.dispatch({type:"Buy_Soap", payload: 10})
