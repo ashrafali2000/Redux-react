6467d14ae60a6f676567d3d8293e710e6aff33ed
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-21 14:08:41 +0500

    update home page

--- redux-toolkit/src/Home.js
+++ redux-toolkit/src/Home.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+
 import './Home.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment,decrement } from './features/sliceCreate';
 
 function Home() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
   return (
-    <div className="App">
-     
+    <div className="Home">
+    <h1>Home Page</h1>
+    <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
